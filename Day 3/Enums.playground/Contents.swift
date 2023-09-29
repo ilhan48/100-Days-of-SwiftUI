331b07457a8a1002ad60e06d98013699251d4a12
ilhan <ilhanodun34@gmail.com>
2023-09-29 16:57:43 +0300

    [ADD]:  enums101

---
+++ Day 3/Enums.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+// Create Enums
+
+enum Weekday {
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+}
+
+enum Months {
+    case January, February, March, April, May, June, July, August, September, October, November, December
+}
+var day = Weekday.monday
+
+day = .friday
+
+var month = Months.April
+

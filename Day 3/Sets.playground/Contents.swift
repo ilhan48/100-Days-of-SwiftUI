339394833f427a5066cf38d806b094a8129bdebf
ilhan <ilhanodun34@gmail.com>
2023-09-29 16:43:17 +0300

    [ADD]: sets101

---
+++ Day 3/Sets.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// FAST for
+
+// Create a set
+let actors = Set(["Denzel", "Tom", "Nicolas", "Samuel"])
+
+// Create a empty set
+var cities = Set<String> ()
+
+// Add item (.insert())
+cities.insert("Mugla")
+// Contains? return true or false
+cities.contains("Mugla")
+
+
